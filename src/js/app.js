fefb430a7ba4e17c1efd767b62e958069e02ee07
tashenoff <kail.dead.kd@gmail.com>
2020-11-30 18:34:53 +0600

    .

--- src/js/app.js
+++ src/js/app.js
@@ -2,72 +2,61 @@ $(function() {
   objectFitImages();
 });
 
-
-
-
-$(".hamburger").on('click', function(){
-    $('.navbar__dropdown').slideToggle('medium', function() {
+$('.hamburger').on('click', function() {
+  $('.navbar__dropdown').slideToggle('medium', function() {
     if ($(this).is(':visible')) {
-        $(this).css('visibility','visible');
-        
-    }         
-  });
-  })
-  
-  
-  $('.hamburger').click(function(e){
-    e.preventDefault();
-    $(this).toggleClass('is-active');
-  });
-  
-  
-  
-  
-
-
-    $('#owl-one').owlCarousel({
-    loop:true,
-    margin:10,
-    dots: false,
-    nav: false,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        600:{
-            items:1,
-            nav:false
-        },
-        1000:{
-            items:1,
-            nav:false,
-            loop:false
-        }
+      $(this).css('display', 'flex');
     }
-})
+  });
+});
 
+$('.hamburger').click(function(e) {
+  e.preventDefault();
+  $(this).toggleClass('is-active');
+});
 
-$('#owl-two').owlCarousel({
-  loop:true,
-  margin:10,
+$('#owl-one').owlCarousel({
+  loop: true,
+  margin: 10,
   dots: false,
   nav: false,
-  responsiveClass:true,
-  responsive:{
-      0:{
-          items:1,
-          nav:false
-      },
-      600:{
-          items:1,
-          nav:false
-      },
-      1000:{
-          items:6,
-          nav:false,
-          loop:false
-      }
-  }
-})
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 1,
+      nav: false,
+    },
+    1000: {
+      items: 1,
+      nav: false,
+      loop: false,
+    },
+  },
+});
+
+$('#owl-two').owlCarousel({
+  loop: true,
+  margin: 10,
+  dots: false,
+  nav: false,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 1,
+      nav: false,
+    },
+    1000: {
+      items: 6,
+      nav: false,
+      loop: false,
+    },
+  },
+});
